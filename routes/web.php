708addad5e21d9ed9d930a6092b620f4028c03b0
anzharnugraha26 <anzharnugraha50@gmail.com>
2022-01-18 14:44:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(
+    ['namespace' => 'AnzharEcommerce\Admin', 'prefix' => 'admin-anzharecommerce'],
+    function () {
+        Route::get('dashboard', 'DashboardController@index');
+    }
+);
